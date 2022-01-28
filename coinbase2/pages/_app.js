7a2b521e210fb55984530d4cd1a39960aa504569
agnxsh <iamagnix@gmail.com>
2022-01-29 04:10:03 +0530

    late nights rockkk

--- coinbase2/pages/_app.js
+++ coinbase2/pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const supportedChainIds = [80001, 4];
+  const connectors = {
+    injected: {},
+  };
+  return (
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+      {/* in this way all of the pages are wrapped in this web3 user authentication */}
+    </ThirdwebWeb3Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
